bedd3622bb5fc731f193a5e5d0586b967cb92e21
ABIGAILSOFIA <ABIGAILSOFIA@Sofy-Luis>
2021-04-30 13:55:32 -0500

    Practica 2: Operaciones Basicas

---
+++ src/g4s22p2rpr/G4s22P2RPR.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p2rpr;
+
+/**
+ *
+ * @author ABIGAILSOFIA
+ */
+public class G4s22P2RPR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
